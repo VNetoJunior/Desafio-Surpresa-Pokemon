f96b6f697ca7582f530811d70f0380e65fc2a24f
VNetoJunior <volneyjunior000@outlook.com>
2024-10-17 11:00:19 -0300

    Projeto em si

---
+++ index.js
@@ -0,0 +1,20 @@
+// NOME, LEVEL, SEXO, COR, MÁXIMO DE VIDA e VIDA ATUAL
+let pokemon = [
+    ["POOCHYENA " , 2 , "M " , "Cinza " , 13 , 13] ,
+    ["ZIGZAGOON " , 2 , "F " , "Marrom " , 13 , 13] ,
+    ["DRAGONITE " , 5 , "M " , "Laranja " , 25 , 25] ,
+    ["DRAGONITE " , 5 , "F " , "Laranja " , 24 , 24] ,
+    ["DRAGONITE " , 5 , "F " , "Laranja " , 24 , 24] ,
+    ["POOCHYENA " , 3 , "F " , "Cinza " , 15 , 15] ,
+    ["WURMPLE " , 2 , "M " , "Rosa " , 14 , 7] ,
+    ]
+    
+    let cadastro = "Cadastrado com sucesso!"
+    
+    console.log(pokemon[0][0] + cadastro)
+    console.log(pokemon[1][0] + cadastro)
+    console.log(pokemon[2][0] + cadastro)
+    console.log(pokemon[3][0] + cadastro)
+    console.log(pokemon[4][0] + cadastro)
+    console.log(pokemon[5][0] + cadastro)
+    console.log(pokemon[6][0] + cadastro)
